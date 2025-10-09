85341daf1fb3689d1f007790e31a8c2e3f76c9ca
ItsKurZa <thanghoang.23122002@gmail.com>
2025-10-09 13:45:43 +0000

    Test backend

--- src/usecases/account/createAccount.js
+++ src/usecases/account/createAccount.js
@@ -1,8 +1,8 @@
 import { createUser } from '../../infrastructure/services/firebase.services.js';
 import { requireFields } from '../../utils/validate.js';
 
-export const createAccount = async ({ email, password, fullName, phone, CCCD }) => {
+export const createAccount = async ({ email, password, fullname, phone, cccd }) => {
     requireFields({ email, password }, ['email', 'password']);
-    const result = await createUser({ email, password, fullName, phone, CCCD });
+    const result = await createUser({ email, password, fullname, phone, cccd });
     return result;
 };
