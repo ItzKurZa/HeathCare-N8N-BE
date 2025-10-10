968554913e3f0208c519a027c04e3fb26f9bb83b
ItsKurZa <thanghoang.23122002@gmail.com>
2025-10-10 04:15:33 +0000

    Update Upload

--- src/interfaces/controllers/file.controller.js
+++ src/interfaces/controllers/file.controller.js
@@ -1,10 +1,10 @@
-import { sendMedicalFileToN8n } from '../../usecases/medfile/uploadMedicalFile.js';
+import { sendMedicalFileToN8nAndCloud } from '../../usecases/medfile/uploadMedicalFile.js';
 
 export const uploadMedical = async (req, res, next) => {
     try {
         const file = req.file;
         const fields = req.body;
-        const result = await sendMedicalFileToN8n({ fields, file });
+        const result = await sendMedicalFileToN8nAndCloud({ fields, file });
         res.status(200).json({ success: true, result });
     } catch (err) {
         next(err);
