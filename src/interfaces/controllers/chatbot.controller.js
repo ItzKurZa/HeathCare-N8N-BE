bf60503402da9a37d995602a55f7fef8c3fddb0b
ItsKurZa <thanghoang.23122002@gmail.com>
2025-10-10 14:05:04 +0000

    Update Chatbot

--- src/interfaces/controllers/chatbot.controller.js
+++ src/interfaces/controllers/chatbot.controller.js
@@ -2,8 +2,8 @@ import { sendMessageToN8n } from '../../usecases/chatbot/sendMessageToN8N.js';
 
 export const sendMessage = async (req, res, next) => {
     try {
-        const { userId, message } = req.body;
-        const result = await sendMessageToN8n({ userId, message });
+        const { user_id, message } = req.body;
+        const result = await sendMessageToN8n({ user_id, message });
         res.status(200).json({ success: true, reply: result });
     } catch (err) {
         next(err);
--- src/usecases/chatbot/sendMessageToN8N.js
+++ src/usecases/chatbot/sendMessageToN8N.js
@@ -2,7 +2,7 @@ import { sendChatMessage } from '../../infrastructure/services/n8n.services.js';
 import { requireFields } from '../../utils/validate.js';
 
 export const sendMessageToN8n = async ({ userId, message }) => {
-    requireFields({ userId, message }, ['userId', 'message']);
+    requireFields({ userId, message }, ['user_id', 'message']);
     const result = await sendChatMessage({ userId, message });
     return result;
 };
