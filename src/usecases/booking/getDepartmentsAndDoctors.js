237ce118b8e424624a19c2070b2fea9fdb75a750
ItsKurZa <thanghoang.23122002@gmail.com>
2025-10-10 15:48:41 +0000

    Get Catalog

--- src/usecases/booking/getDepartmentsAndDoctors.js
+++ src/usecases/booking/getDepartmentsAndDoctors.js
@@ -1,4 +1,4 @@
-import { fetchDepartmentsAndDoctors } from '../../services/n8n.services.js';
+import { fetchDepartmentsAndDoctors } from '../../infrastructure/services/n8n.services.js';
 
 export const getDepartmentsAndDoctorsService = async () => {
   return await fetchDepartmentsAndDoctors();
