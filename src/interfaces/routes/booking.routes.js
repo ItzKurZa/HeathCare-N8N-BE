7ceda36182fe5964b45c15734810344805fdae84
ItsKurZa <thanghoang.23122002@gmail.com>
2025-12-19 12:44:09 +0000

    Finishing Profile

--- src/interfaces/routes/booking.routes.js
+++ src/interfaces/routes/booking.routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { submitBooking } from '../controllers/booking.controller.js';
 import { getDepartmentsAndDoctors } from '../controllers/booking.controller.js';
+import { requireAuth } from '../../infrastructure/middlewares/auth.middleware.js';
+import { cancelBooking } from '../controllers/booking.controller.js';
 
 const router = express.Router();
 
