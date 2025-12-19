e0bd8ce64f61312d87e59e9326d27862f41d5c10
ItsKurZa <thanghoang.23122002@gmail.com>
2025-12-19 06:04:34 +0000

    Open Public CORS

--- src/app.js
+++ src/app.js
@@ -10,12 +10,7 @@ import { errorHandler } from './infrastructure/middlewares/errorHandler.js';
 
 const app = express();
 
-// const allowedOrigins = [
-//     config.frontendUrl, // domain thật (https://kurza.id.vn)
-//     "https://orange-space-yodel-x5xjx4pv5p4q2pjp6-5173.app.github.dev/",
-// ];
-
-// app.use(cors({ origin: allowedOrigins }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/booking', bookingRoutes);
