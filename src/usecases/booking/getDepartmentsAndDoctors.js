8b38ab67b5d4046d0d2325d9129559f58c691ac3
ItsKurZa <thanghoang.23122002@gmail.com>
2025-12-19 06:55:56 +0000

    Restruture Database

--- src/usecases/booking/getDepartmentsAndDoctors.js
+++ src/usecases/booking/getDepartmentsAndDoctors.js
@@ -1,4 +1,4 @@
-import { getDepartmentsAndDoctorsFromFirestore } from "../../infrastructure/services/firebase.services";
+import { getDepartmentsAndDoctorsFromFirestore } from "../../infrastructure/services/firebase.services.js";
 
 export const getDepartmentsAndDoctorsService = async () => {
   const results = await getDepartmentsAndDoctorsFromFirestore();
