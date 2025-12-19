47abe7b2b8f701db09c8d95fec56cfcada0dfc26
ItsKurZa <thanghoang.23122002@gmail.com>
2025-12-19 09:28:48 +0000

    Rework Uploading

--- src/interfaces/routes/file.routes.js
+++ src/interfaces/routes/file.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import multer from 'multer';
-import { uploadMedical } from '../controllers/file.controller.js';
+import { uploadFile } from '../controllers/file.controller.js';
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 100 * 1024 * 1024 } });
 
-router.post('/upload', upload.single('file'), uploadMedical);
+router.post('/upload', upload.single('file'), uploadFile);
 
 export default router;
